ee6458250b327884e6d056e57d169493226598b0
Tobi Adeleke <tobiaadeleke@gmail.com>
2025-11-24 23:07:19 +0000

    fix icons

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -1,5 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+})
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
